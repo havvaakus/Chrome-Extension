397acba329cd1e99d942f586a66136b5c2fe0a3b
Havva Kuş <havvaakus@gmail.com>
2023-12-02 20:58:15 +0300

    Delete Button

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i< leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+        `
+    
+    }
+    ulEl.innerHTML = listItems
+}
 
 deleteBtn.addEventListener("dblclick", function()
 {
@@ -16,18 +37,4 @@ inputBtn.addEventListener("click", function() {
     renderLeads()
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i< myLeads.length; i++) {
-        listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-        `
-    
-    }
-    ulEl.innerHTML = listItems
-}
 
