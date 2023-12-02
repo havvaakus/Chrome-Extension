7328ac2d9de6e1370ba1acee44ade7d624dce062
Havva Kuş <havvaakus@gmail.com>
2023-12-02 20:46:51 +0300

    delete button

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
+deleteBtn.addEventListener("dblclick", function()
+{
+    localStorage.clear()
+    myLeads[]
+    renderLeads()
+})
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
